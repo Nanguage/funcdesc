5e2fbf4e2d6476b3bf95584891cacd136368b002
nanguage <nanguage@yahoo.com>
2023-02-07 22:47:45 +0800

    add checkers to Value

--- funcdesc/desc.py
+++ funcdesc/desc.py
@@ -11,7 +11,14 @@ NotDef = _NotDef()
 T1 = T.TypeVar("T1")
 
 
+TypeChecker = T.Callable[[T.Any, type], bool]
+RangeChecker = T.Callable[[T.Any, T.Any], bool]
+
+
 class Value(T.Generic[T1]):
+    type_to_range_checker: T.Dict[type, TypeChecker] = {}
+    type_to_type_checker: T.Dict[type, RangeChecker] = {}
+        
     def __init__(
             self,
             type_: T.Optional[T.Type[T1]] = None,
@@ -23,6 +30,54 @@ class Value(T.Generic[T1]):
         self.type = type_
         self.range = range_
         self.default = default
+
+    @property
+    def type_checker(self):
+        return self.type_to_type_checker.get(self.type.__name__, None)
+
+    @property
+    def range_checker(self):
+        return self.type_to_range_checker.get(self.type.__name__, None)
+
+    @classmethod
+    def register_range_check(cls, type, checker):
+        cls.type_to_range_checker[type.__name__] = checker
+
+    @classmethod
+    def register_type_check(cls, type, checker=None):
+        checker = checker or (lambda v, t: isinstance(v, t))
+        cls.type_to_type_checker[type.__name__] = checker
+
+    def check_type(self, val):
+        if self.type_checker is not None:
+            if not self.type_checker(val, self.type):
+                raise TypeError(
+                    f"Value {val} is not in valid type({self.type})")
+
+    def check_range(self, val):
+        if (self.range_checker is not None):
+            if (not self.range_checker(val, self.range)):
+                raise ValueError(f"Value {val} is not in a valid range({self.range}).")
+
+    def check(self, val):
+        if (self.range_checker is None) and (self.type_checker is None):
+            raise NotImplementedError(
+                f"Not checker registered for type: {type}")
+        self.check_type(val)
+        self.check_range(val)
+
+
+# register basic types
+def _check_number_in_range(v, range):
+    return (range is None) or (range[0] <= v <= range[1])
+
+
+Value.register_type_check(str)
+Value.register_range_check(int, _check_number_in_range)
+Value.register_type_check(int)
+Value.register_range_check(float, _check_number_in_range)
+Value.register_type_check(float)
+Value.register_type_check(bool)
 
 
 class SideEffect():
